2c69cb1a11a01841c4c7b7fa9b480cea90f6d557
RJSaraf <rajasaraf@hotmail.com>
2021-11-14 18:50:32 +0530

    new heroku settings

--- Quantum_django/settings.py
+++ Quantum_django/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os # new
+import django_heroku
+import dj_database_url
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -201,3 +205,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+
+django_heroku.settings(locals())
